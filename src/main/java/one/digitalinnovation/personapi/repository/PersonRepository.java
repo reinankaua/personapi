5b22e65438bcf5e342ba3dfed2e35c771993a420
Reinan Kauã <contato.reinankaua@gmail.com>
2021-10-07 18:33:31 -0300

    New commit

--- src/main/java/one/digitalinnovation/personapi/repository/PersonRepository.java
+++ src/main/java/one/digitalinnovation/personapi/repository/PersonRepository.java
@@ -3,5 +3,5 @@ package one.digitalinnovation.personapi.repository;
 import one.digitalinnovation.personapi.entity.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PersonRepository extends JpaRepository<Person, Long> {
+public interface    PersonRepository extends JpaRepository<Person, Long> {
 }
